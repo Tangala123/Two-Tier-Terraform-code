431e2fb6625ebc6980a9a53e22b977b115e989cb
Tangala123 <98816309+Tangala123@users.noreply.github.com>
2025-12-16 22:01:11 +0530

    Update network.tf

--- infra/network.tf
+++ infra/network.tf
@@ -15,6 +15,10 @@ resource "aws_internet_gateway" "main" {
 # Create route table for public subnet
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
+  tags   = { 
+    Name = "${var.project_name}-pub-rt"
+  }
+}
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.main.id
@@ -39,6 +43,10 @@ resource "aws_route_table_association" "public" {
 # Create route table for private subnet
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
+}
+ tags   = { 
+   Name = "${var.project_name}-private-rt" 
+ }
 }
 resource "aws_subnet" "private" {
   count             = var.subnet_count

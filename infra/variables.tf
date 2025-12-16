6339c5ad6abb1ac067332d70aa3854f195a992db
Tangala123 <98816309+Tangala123@users.noreply.github.com>
2025-12-16 15:07:10 +0530

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -14,39 +14,28 @@ variable "env" {
   default = "dev"
 }
 
+# VPC CIDR
 variable "vpc_cidr" {
   type    = string
   default = "172.31.0.0/16"
 }
 
-variable "az1" {
-  type    = string
-  default = "ap-southeast-2a"
+# ✅ Availability Zones as a list of strings
+variable "availability_zones" {
+  type    = list(string)
+  default = ["ap-southeast-2a", "ap-southeast-2b"]
 }
 
-variable "az2" {
-  type    = string
-  default = "ap-southeast-2b"
+# ✅ Public subnets as a list
+variable "public_subnets" {
+  type    = list(string)
+  default = ["172.31.1.0/24", "172.31.2.0/24"]
 }
 
-variable "pub1_cidr" {
-  type    = string
-  default = "172.31.1.0/24"
-}
-
-variable "pub2_cidr" {
-  type    = string
-   default = "172.31.2.0/24"
-}
-
-variable "priv1_cidr" {
-  type    = string
-  default = "172.31.11.0/24"
-}
-
-variable "priv2_cidr" {
-  type    = string
-  default = "172.31.12.0/24"
+# ✅ Private subnets as a list
+variable "private_subnets" {
+  type    = list(string)
+  default = ["172.31.11.0/24", "172.31.12.0/24"]
 }
 
 # Compute
@@ -64,4 +53,4 @@ variable "db_username" {
 variable "db_password" {
   type    = string
   default = "Admin*0987654"
-
+}

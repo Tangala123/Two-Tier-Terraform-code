4b818e11642cacaee61a54d43de7674f49e34d05
Tangala123 <98816309+Tangala123@users.noreply.github.com>
2025-12-16 19:30:58 +0530

    Update data.tf

--- infra/data.tf
+++ infra/data.tf
@@ -4,3 +4,12 @@ data "aws_availability_zones" "azs" {
 data "aws_caller_identity" "current" {
 
 }
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}

da790998ea9e77b5162526fc1641ff7812ffc4d4
Tangala123 <98816309+Tangala123@users.noreply.github.com>
2025-12-16 20:08:27 +0530

    Update rds.tf

--- infra/rds.tf
+++ infra/rds.tf
@@ -1,5 +1,5 @@
 resource "aws_db_subnet_group" "db" {
-  name       = "rds-subnet-group"
+  name       = "${var.project_name}-rds-subnet-group"
   subnet_ids = var.sub_ids
 
   tags = {
@@ -8,15 +8,16 @@ resource "aws_db_subnet_group" "db" {
 }
 # Create RDS Instance 
 resource "aws_db_instance" "default" {
-  allocated_storage      = var.storage
+  allocated_storage      = var.rds_allocated_storage_gb
   db_name                = var.db_name
   engine                 = "mysql"
-  engine_version         = "5.7"
-  instance_class         = "db.t3.micro"
-  username               = var.username
-  password               = var.password
+  engine_version         = var.mysql_engine_version
+  instance_class         = var.rds_instance_class
+  username               = var.db_username
+  password               = var.db_password
   parameter_group_name   = "default.mysql5.7"
   skip_final_snapshot    = true
+  publicly_accessible    = false
   db_subnet_group_name   = aws_db_subnet_group.db.name
   vpc_security_group_ids = [aws_security_group.rds.id]
 }

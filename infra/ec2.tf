bdf31d18952d9875a92d49a3d805075cc92a53dd
Tangala123 <98816309+Tangala123@users.noreply.github.com>
2025-12-16 22:15:43 +0530

    Update ec2.tf

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "main" {
   ami                         = data.aws_ami.amazon_linux_2.id
   associate_public_ip_address = var.public_ip
   instance_type               = var.instance_type
-  subnet_id                   = var.subnet_ids[count.index]
+  subnet_id                   = aws_subnet.public.*.id[count.index]
   vpc_security_group_ids      = [aws_security_group.web.id]
   key_name                    = var.key_name
   user_data = file("./scripts/httpd.sh")

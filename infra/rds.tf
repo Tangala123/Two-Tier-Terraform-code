5f53bd5ad1aef379cf8f1c50919aa32451f1b97b
Tangala123 <98816309+Tangala123@users.noreply.github.com>
2025-12-16 19:57:49 +0530

    Update rds.tf

--- infra/rds.tf
+++ infra/rds.tf
@@ -20,33 +20,3 @@ resource "aws_db_instance" "default" {
   db_subnet_group_name   = aws_db_subnet_group.db.name
   vpc_security_group_ids = [aws_security_group.rds.id]
 }
-# Security Group
-resource "aws_security_group" "rds" {
-  name        = "allow traffic for RDS"
-  description = "Allow inbound traffic"
-  vpc_id      = var.vpc_id
-
-  dynamic "ingress" {
-    for_each = var.rds_ingress_rules
-    content {
-      description     = "some description"
-      from_port       = ingress.value.port
-      to_port         = ingress.value.port
-      protocol        = ingress.value.protocol
-      cidr_blocks     = ingress.value.cidr_blocks
-      security_groups = ingress.value.security_groups
-    }
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "rds_security_rules"
-  }
-}

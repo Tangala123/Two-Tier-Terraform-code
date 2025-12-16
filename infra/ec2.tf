b851c09e609284df49d5f62c0f56980538b9d0a1
Tangala123 <98816309+Tangala123@users.noreply.github.com>
2025-12-16 19:40:59 +0530

    Update ec2.tf

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "main" {
   count                       = var.ec2_count
-  ami                         = var.ami
+  ami                         = data.aws_ami.amazon_linux_2.id
   associate_public_ip_address = var.public_ip
   instance_type               = var.instance_type
   subnet_id                   = var.subnet_ids[count.index]
@@ -10,35 +10,5 @@ resource "aws_instance" "main" {
   user_data_replace_on_change = true
   tags = {
     Name = "terraform-training[count.index]"
-  }
-}
-# Create security group
-
-resource "aws_security_group" "web" {
-  name        = "allow traffic for web"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = var.vpc_id
-
-  dynamic "ingress" {
-    for_each = var.web_ingress_rules
-    content {
-      description = "some description"
-      from_port   = ingress.value.port
-      to_port     = ingress.value.port
-      protocol    = ingress.value.protocol
-      cidr_blocks = ingress.value.cidr_blocks
-    }
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "web_security_rules"
   }
 }

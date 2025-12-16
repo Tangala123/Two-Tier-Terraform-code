26386059aa5405dc555097910b07874eb7eba903
Tangala123 <98816309+Tangala123@users.noreply.github.com>
2025-12-16 19:51:03 +0530

    Update sg.tf

--- infra/sg.tf
+++ infra/sg.tf
@@ -1,1 +1,74 @@
 
+resource "aws_security_group" "web" {
+  name        = "web-sg"
+  description = "Allow SSH, HTTP, and HTTPS inbound traffic"
+  vpc_id      = var.vpc_id
+
+  # SSH
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTP
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTPS
+  ingress {
+    description = "Allow HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Outbound traffic (all)
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "web_security_rules"
+  }
+
+##RDS sg
+resource "aws_security_group" "rds" {
+  name        = "rds-sg"
+  description = "Allow MySQL access from Web SG"
+  vpc_id      = var.vpc_id
+
+  # Allow MySQL from Web SG
+  ingress {
+    description     = "MySQL from Web SG"
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.web.id]
+  }
+
+  # Outbound traffic (all)
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "rds_security_rules"
+  }
+

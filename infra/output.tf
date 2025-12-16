7cf4b385124ffde823b8def733611a8b3645debe
Tangala123 <98816309+Tangala123@users.noreply.github.com>
2025-12-16 16:12:33 +0530

    Update output.tf

--- infra/output.tf
+++ infra/output.tf
@@ -1,1 +1,15 @@
-
+output "security_group_id" {
+  value = aws_security_group.web.id
+}
+output "instance_ids" {
+  value = aws_instance.main.*.id
+}
+output "pri_sub_ids" {
+  value = local.pri_sub_ids
+}
+output "pub_sub_ids" {
+  value = local.pub_sub_ids
+}
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
